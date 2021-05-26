fcd71c372fcadb7599238b1eb3e40b3e44d5ae9d
Damien de Lartigue <damien@delartigue.co.uk>
2021-05-26 12:15:18 +0000

    Automatically backed up by Learn

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 export function manageFriends(state={friends:[]}, action){
   
   switch(action.type) {
@@ -9,6 +10,13 @@ export function manageFriends(state={friends:[]}, action){
     case "REMOVE_FRIEND":
       const newFriends = state.friends.filter(friend => friend.id != action.id)
       return {friends: newFriends}
+=======
+export function manageFriends(state, action){
+  
+  switch(action.type) {
+    case "ADD_FRIEND":
+      return {friends: state.friends.push(action.friend)
+>>>>>>> a91fe94757be74db7f69fa62bac12062207ccd48
       break
     default:
       return state
